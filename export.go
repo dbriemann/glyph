c75eabaa5ee96415cf9447a63d157f0585e775fa
David Linus Briemann <dlb@mailbox.org>
2018-03-14 21:12:40 +0100

    switches to mustache for templating

--- export.go
+++ export.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"html/template"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 
 	"github.com/google/go-github/github"
 	"github.com/gosimple/slug"
-	"github.com/microcosm-cc/bluemonday"
-	blackfriday "gopkg.in/russross/blackfriday.v2"
+	"github.com/hoisie/mustache"
+	gfm "github.com/shurcooL/github_flavored_markdown"
 )
 
 type Issue struct {
 	Title   string
 	Link    string // slugified title
-	Content template.HTML
-	Summary template.HTML
+	Content string
+	Summary string
 }
 
 func prepareIssues(issues []*github.Issue) ([]Issue, error) {
@@ -28,10 +26,8 @@ func prepareIssues(issues []*github.Issue) ([]Issue, error) {
 			Link:  slug.Make(issue.GetTitle()) + ".html",
 		}
 		if exIssue.Title != "" {
-			unsafe := blackfriday.Run([]byte(issue.GetBody()))
-			html := string(bluemonday.UGCPolicy().SanitizeBytes(unsafe))
-			exIssue.Content = template.HTML(html)
-			if len(exIssue.Content) > 200 {
+			exIssue.Content = string(gfm.Markdown([]byte(issue.GetBody())))
+			if len(exIssue.Content) > 200 { // TODO
 				exIssue.Summary = exIssue.Content[:200]
 			} else {
 				exIssue.Summary = exIssue.Content
@@ -66,45 +62,23 @@ func BuildSite(issues []*github.Issue, cfg *Config) error {
 }
 
 func exportIssue(issue Issue, cfg *Config) error {
-	raw, err := ioutil.ReadFile("tmpl/issue.tmpl")
-	if err != nil {
-		return err
-	}
-	itmpl, err := template.New(issue.Title).Parse(string(raw))
-	if err != nil {
-		return err
-	}
-
 	data := struct {
 		TITLE         string
 		ISSUE_TITLE   string
-		ISSUE_CONTENT template.HTML
+		ISSUE_CONTENT string
 	}{
 		TITLE:         cfg.Site.Title,
 		ISSUE_TITLE:   issue.Title,
 		ISSUE_CONTENT: issue.Content,
 	}
 
-	f, err := os.Create(filepath.Join(cfg.Repository.TargetDir, issue.Link))
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	err = itmpl.Execute(f, data)
+	issueTmpl := mustache.RenderFileInLayout("tmpl/issue.mustache", "tmpl/layout.mustache", data)
+	outname := filepath.Join(cfg.Repository.TargetDir, issue.Link)
+	err := ioutil.WriteFile(outname, []byte(issueTmpl), 0755)
 	return err
 }
 
 func exportIndex(issues []Issue, cfg *Config) error {
-	raw, err := ioutil.ReadFile("tmpl/index.tmpl")
-	if err != nil {
-		return err
-	}
-	index, err := template.New("index").Parse(string(raw))
-	if err != nil {
-		return err
-	}
-
 	data := struct {
 		TITLE  string
 		ISSUES []Issue
@@ -112,13 +86,8 @@ func exportIndex(issues []Issue, cfg *Config) error {
 		TITLE:  cfg.Site.Title,
 		ISSUES: issues,
 	}
-
-	f, err := os.Create(filepath.Join(cfg.Repository.TargetDir, "index.html"))
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	err = index.Execute(f, data)
+	indexTmpl := mustache.RenderFileInLayout("tmpl/index.mustache", "tmpl/layout.mustache", data)
+	outname := filepath.Join(cfg.Repository.TargetDir, "index.html")
+	err := ioutil.WriteFile(outname, []byte(indexTmpl), 0755)
 	return err
 }
